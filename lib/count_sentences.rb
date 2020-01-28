11c9da66d883640d3b367d1868cefba0c84b8224
Doak Covington <doakcovington@protonmail.com>
2020-01-28 03:00:31 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    if self.end_with?(".","?","!") && self.length >= 2
+    if self.end_with?(".","?","!") && self.length > 2
       sentence_array = self.split(/[!?.]/)
     end
     sentence_array
